bc874cd595125f573644428ead1dca5eacd97bee
Chris Fung <aergonaut@gmail.com>
2024-06-25 21:21:44 -0700

    add tests

--- spec/pseudo_l10n/transformer_spec.rb
+++ spec/pseudo_l10n/transformer_spec.rb
@@ -2,16 +2,31 @@ module PseudoL10n
   RSpec.describe Transformer do
     describe ".call" do
       context "when given a string" do
-        let(:original) { "Hello world" }
-
         it "surrounds the original message with the marker character" do
-          expect(Transformer.call(original)).to start_with(Transformer::MARKER)
-          expect(Transformer.call(original)).to end_with(Transformer::MARKER)
+          expect(Transformer.call("Hello world")).to start_with(Transformer::MARKER)
+          expect(Transformer.call("Hello world")).to end_with(Transformer::MARKER)
+        end
+
+        it "lengthens the string by replacing half-width characters with full-width" do
+          expect(Transformer.call("Hello world")).to eq("√Ｈｅｌｌｏ ｗｏｒｌｄ√")
+        end
+
+        it "does not change HTML tags" do
+          expect(Transformer.call("<p>Hello world</p>")).to eq("√<p>Ｈｅｌｌｏ ｗｏｒｌｄ</p>√")
+        end
+
+        it "does not change interpolation variables" do
+          expect(Transformer.call("Hello %{world}")).to eq("√Ｈｅｌｌｏ %{world}√")
+        end
+
+        it "does not change URLs" do
+          expect(Transformer.call("http://example.com")).to eq("√http://example.com√")
         end
       end
 
       context "when given a Hash" do
         let(:original) { { foo: :bar, x: "hello world" } }
+
         subject { Transformer.call(original) }
 
         it "transforms keys that lead to Strings" do
